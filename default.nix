ec94688d176d1a0ecaf810010c06e1a2050413bc
nando <nando@hex7.com>
2021-01-23 07:57:23 -0500

    fixup nix-shell

--- default.nix
+++ default.nix
@@ -1,11 +1,20 @@
 with import <nixpkgs> {};
+#with pkgs.python38Packages;
+
 stdenv.mkDerivation {
   name = "covid19";
   buildInputs = [
-    gcc6
-    ncurses
-    libstdcxx5
-    stdenv.cc.cc.lib
-    libgcc
+    stdenv
+    python38Packages.pip
+    python38Packages.virtualenv
   ];
+  src = null;
+  shellHook = ''
+    export GIT_SSL_CAINFO=/etc/ssl/certs/ca-bundle.crt
+    export LD_LIBRARY_PATH=${stdenv.cc.cc.lib}/lib
+    SOURCE_DATE_EPOCH=$(date +%s)
+    virtualenv --no-setuptools venv
+    source venv/bin/activate
+    pip install -r requirements.txt --no-cache-dir
+  '';
 }
